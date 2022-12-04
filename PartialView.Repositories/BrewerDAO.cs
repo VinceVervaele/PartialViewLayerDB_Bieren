5e96bd57804ce471954c5d5a27e77e317e6c1827
VinceVervaele <vince.vervaele@student.vives.be>
2022-12-04 15:56:29 +0100

    Testing

---
+++ PartialView.Repositories/BrewerDAO.cs
@@ -0,0 +1,40 @@
+﻿using PartialView.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialView.Repositories
+{
+    public class BrewerDAO
+    {
+        private readonly BierDBcontext _dbContext;
+
+        public BrewerDAO()
+        {
+            _dbContext = new BierDBcontext();
+        }
+
+        public IEnumerable<Models.Entities.Brewery>? GetAll()
+        {
+            //SQL "select * from Adult"
+
+            try
+            {
+                return _dbContext.Breweries.ToList();
+            }
+            catch (Microsoft.Data.SqlClient.SqlException ex)
+            {
+                Debug.WriteLine("Didn't found db", ex.ToString());
+                return null;
+            }
+
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
