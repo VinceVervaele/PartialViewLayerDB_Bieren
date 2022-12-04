5cead7a036ab878ecbe24d960bdb402e2d7b126b
VinceVervaele <vince.vervaele@student.vives.be>
2022-12-04 15:38:01 +0100

    Imapper

---
+++ PartialViewLayerDB_Bieren/AutoMapper/AutoMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PartialView.Models.Entities;
+using PartialViewLayerDB_Bieren.ViewModels;
+
+namespace PartialViewLayerDB_Bieren.AutoMapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Beer, BierVM>();
+        }
+    }
+}
